f00d350aead5e7fd0182a7d1df5ecc2d709a0ab9
Rafael Abensur <abensur7@gmail.com>
2017-05-05 17:00:22 -0300

    add CNAME file

--- config/index.js
+++ config/index.js
@@ -7,7 +7,7 @@ module.exports = {
     index: path.resolve(__dirname, '../docs/index.html'),
     assetsRoot: path.resolve(__dirname, '../docs'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: 'https://cdn.rawgit.com/abensur/terraria-progress/0ea3d50f/docs/',
+    assetsPublicPath: '/',
     productionSourceMap: true,
     // Gzip off by default as many popular static hosts such as
     // Surge or Netlify already gzip all static assets for you.
